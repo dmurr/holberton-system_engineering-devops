437720425ff885b28017e3bbb136c99a00ea1bd4
dmurr <diegocmurray@gmail.com>
2019-04-05 01:12:01 +0000

    Removed returns and fixed syntax

--- 0x0D-configuration_management/2-execute_a_command.pp
+++ 0x0D-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # Create a manifest that kills a process named killmenow
 exec { 'kills process':
-  command => "pkill -f 'killmenow'",
+  command => 'pkill -f killmenow',
   path    => ['/usr/bin', '/usr/sbin',],
-  returns => [0],
 }
