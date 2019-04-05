409e3b077025a4a916c71dc177ba9cc8e27a7548
dmurr <diegocmurray@gmail.com>
2019-04-05 01:06:08 +0000

    Kills process named killmenow

---
+++ 0x0D-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# Create a manifest that kills a process named killmenow
+exec { 'kills process':
+  command => "pkill -f 'killmenow'",
+  path    => ['/usr/bin', '/usr/sbin',],
+  returns => [0],
+}
